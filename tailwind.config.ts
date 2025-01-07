833faa484ef84c3706b65ee662b10bd7315da92c
OsmanTunahan <osmantunahan007@gmail.com>
2025-01-07 18:11:23 +0300

    minor: update css and configuration (#3)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,51 +7,95 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      typography: {
-        DEFAULT: {
-          css: {
-            color: '#fff',
-            a: {
-              color: '#3b82f6',
-              '&:hover': {
-                color: '#60a5fa',
-              },
-            },
-            'h1, h2, h3, h4': {
-              color: '#fff',
-            },
-            blockquote: {
-              borderLeftColor: '#3f3f46',
-              color: '#d4d4d8',
-            },
-            hr: {
-              borderColor: '#3f3f46',
-            },
-            strong: {
-              color: '#fff',
-            },
-            code: {
-              color: '#fff',
-            },
-            pre: {
-              backgroundColor: '#18181b',
-              code: {
-                color: '#fff',
-              },
-            },
-          },
-        },
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		typography: {
+  			DEFAULT: {
+  				css: {
+  					color: '#fff',
+  					a: {
+  						color: '#3b82f6',
+  						'&:hover': {
+  							color: '#60a5fa'
+  						}
+  					},
+  					'h1, h2, h3, h4': {
+  						color: '#fff'
+  					},
+  					blockquote: {
+  						borderLeftColor: '#3f3f46',
+  						color: '#d4d4d8'
+  					},
+  					hr: {
+  						borderColor: '#3f3f46'
+  					},
+  					strong: {
+  						color: '#fff'
+  					},
+  					code: {
+  						color: '#fff'
+  					},
+  					pre: {
+  						backgroundColor: '#18181b',
+  						code: {
+  							color: '#fff'
+  						}
+  					}
+  				}
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
   plugins: [
     require('@tailwindcss/typography'),
-  ],
-  darkMode: 'class',
+      require("tailwindcss-animate")
+],
+  darkMode: ['class', "class"],
 };
 export default config;
