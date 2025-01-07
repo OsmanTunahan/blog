0530e4462a9758361ee0c3f6d9cfadbcd448ce6d
OsmanTunahan <osmantunahan007@gmail.com>
2025-01-07 18:33:54 +0300

    minor: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,48 @@ import "@/assets/globals.css";
 import { Providers } from "./providers";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://blog.osmantunahan.com.tr"),
   title: "Osman Tunahan ARIKAN's Blog",
-  description: "Hello, I'm Osman Tunahan ARIKAN. I'm a full stack developer.",
+  description:
+    "Hey, my name is Osman Tunahan ARIKAN. I am a Cyber Security Expert and Full Stack Developer. This is my blog.",
+  openGraph: {
+    title: "Osman Tunahan ARIKAN's Blog",
+    description:
+      "Hey, my name is Osman Tunahan ARIKAN. I am a Cyber Security Expert and Full Stack Developer. This is my blog.",
+    url: "https://blog.osmantunahan.com.tr",
+    siteName: "Osman Tunahan ARIKAN",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: "Osman Tunahan ARIKAN",
+    card: "summary_large_image",
+  },
+  keywords: [
+    "Osman Tunahan ARIKAN",
+    "Osman Tunahan",
+    "ARIKAN",
+    "Osman ARIKAN",
+    "Osman",
+    "Tunahan",
+    "Cyber Security",
+    "Expert",
+    "Full Stack",
+    "Developer",
+    "Blog",
+    "Cyber Security Blog",
+    "Developer Blog"
+  ],
 };
 
 export default function RootLayout({
