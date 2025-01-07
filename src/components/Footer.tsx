78a66114dd7fff35d19f373133da8be4a4070120
OsmanTunahan <osmantunahan007@gmail.com>
2025-01-07 18:37:01 +0300

    minor: small changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="border-t border-zinc-800 py-6 px-4">
       <div className="container mx-auto max-w-4xl text-center text-zinc-400">
-        <p>© 2025 Osman Tunahan ARIKAN. Tüm hakları saklıdır.</p>
+        <p>© 2025 Osman Tunahan ARIKAN. All rights reserved.</p>
       </div>
     </footer>
   );
