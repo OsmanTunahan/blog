f8985281a25bb5e8818148e2c04ad7820efc915b
OsmanTunahan <osmantunahan007@gmail.com>
2025-01-08 14:14:49 +0300

    minor: update database (#4)

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -8,32 +8,44 @@ if (!MONGODB_URI) {
   );
 }
 
-let cached = (global as any).mongoose;
-
-if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null };
+interface MongooseCache {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
 }
 
-async function dbConnect() {
+declare global {
+  var mongoose: { conn: null | typeof mongoose; promise: null | Promise<typeof mongoose> } | undefined;
+}
+
+let cached: MongooseCache = global.mongoose || { conn: null, promise: null };
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectDB() {
   if (cached.conn) {
     return cached.conn;
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose
-      .connect(MONGODB_URI!, {
-        dbName: "blog",
-      })
-      .then(() => mongoose);
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
+      return mongoose;
+    });
   }
 
   try {
     cached.conn = await cached.promise;
-    return cached.conn;
   } catch (e) {
     cached.promise = null;
     throw e;
   }
+
+  return cached.conn;
 }
 
-export default dbConnect;
+export default connectDB;
