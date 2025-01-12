f3bacfe0179d108d0406604cf89d0b7303d9e499
OsmanTunahan <osmantunahan007@gmail.com>
2025-01-12 16:39:04 +0300

    fix: robots.txt index problem

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/auth/*', '/api/*', '/admin/*'],
+      disallow: ['/api/*', '/admin/*'],
     },
     sitemap: `${BASE_URL}/sitemap.xml`,
   };
